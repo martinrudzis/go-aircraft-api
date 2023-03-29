c70ed2e547b543347e705ddfbdf41306d69d3c91
Martin Rudzis <m19rudzis@gmail.com>
2023-03-29 15:46:46 -0700

    Converted output to json format

--- data/data.go
+++ data/data.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 
@@ -9,28 +10,30 @@ import (
 )
 
 type AircraftData struct {
-	icao24, owner, city, state, aircraftMake, aircraftModel, yearBuilt, airworthyDate, weightClass, 
-	engineCount, seatCount, engineCategory, engineMake, engineModel, horsepower, thrust string
-}
-
-
-func printAircraftData(aircraft *AircraftData) {
-	fmt.Printf("icao24: %s\n" + "owner: %s\n" + "city: %s\n" + "state: %s\n" + "aircraft make: %s\n" + 
-	"aircraft model: %s\n" + "year built: %s\n" + "airworthy date: %s\n" + "weight class: %s\n" + 
-	"engine count: %s\n" + "seat count: %s\n" + "engine category: %s\n" + "engine make: %s\n" + 
-	"engine model: %s\n" + "horsepower: %s\n" + "thrust: %s\n", 
-	aircraft.icao24, aircraft.owner, aircraft.city, aircraft.state, aircraft.aircraftMake, 
-	aircraft.aircraftModel, aircraft.yearBuilt, aircraft.airworthyDate, aircraft.weightClass, 
-	aircraft.engineCount, aircraft.seatCount, aircraft.engineCategory,aircraft.engineMake, 
-	aircraft.engineModel, aircraft.horsepower, aircraft.thrust)
+	Icao24         string `json:"icao24"`
+    Owner          string `json:"owner"`
+    City           string `json:"city"`
+    State          string `json:"state"`
+    AircraftMake   string `json:"aircraftMake"`
+    AircraftModel  string `json:"aircraftModel"`
+    YearBuilt      string `json:"yearBuilt"`
+    AirworthyDate  string `json:"airworthyDate"`
+    WeightClass    string `json:"weightClass"`
+    EngineCount    string `json:"engineCount"`
+    SeatCount      string `json:"seatCount"`
+    EngineCategory string `json:"engineCategory"`
+    EngineMake     string `json:"engineMake"`
+    EngineModel    string `json:"engineModel"`
+    Horsepower     string `json:"horsepower"`
+    Thrust         string `json:"thrust"`
 }
 
 func loadAircraftDataFromRow(row *sql.Rows, aircraft *AircraftData) {
 	if row.Next() { 
-		if err := row.Scan(&aircraft.icao24, &aircraft.owner, &aircraft.city, &aircraft.state, 
-			&aircraft.aircraftMake, &aircraft.aircraftModel, &aircraft.yearBuilt, &aircraft.airworthyDate, 
-			&aircraft.weightClass, &aircraft.engineCount, &aircraft.seatCount, &aircraft.engineCategory, 
-			&aircraft.engineMake, &aircraft.engineModel, &aircraft.horsepower, &aircraft.thrust); 
+		if err := row.Scan(&aircraft.Icao24, &aircraft.Owner, &aircraft.City, &aircraft.State, 
+			&aircraft.AircraftMake, &aircraft.AircraftModel, &aircraft.YearBuilt, &aircraft.AirworthyDate, 
+			&aircraft.WeightClass, &aircraft.EngineCount, &aircraft.SeatCount, &aircraft.EngineCategory, 
+			&aircraft.EngineMake, &aircraft.EngineModel, &aircraft.Horsepower, &aircraft.Thrust); 
 			err != nil {
 				log.Fatal(err)
 		}
@@ -62,5 +65,10 @@ func GetAircraftData(input string) {
 
 	loadAircraftDataFromRow(row, &aircraft)
 
-	printAircraftData(&aircraft)
+	jsonData, err := json.Marshal(aircraft)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(jsonData))
 }
